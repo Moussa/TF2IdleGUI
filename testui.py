e74375bf30c25c909227199a80dd529fb5d6f44d
Moussekateer <moussa.uk@gmail.com>
2012-01-09 05:39:19 +0000

    Added groups functionality plus a ton of things I cant remember

--- testui.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'test.ui'
-#
-# Created: Mon Jan 09 02:42:05 2012
-#      by: PyQt4 UI code generator 4.9
-#
-# WARNING! All changes made in this file will be lost!
-
