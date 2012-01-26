1213f178ec62a7479229216388a80a6275f35ef5
Moussekateer <moussa.uk@gmail.com>
2012-01-26 23:55:44 +0000

    updating steamodd, fixing bug when saving to text file

--- DropLogView.py
+++ DropLogView.py
@@ -10,6 +10,29 @@ def returnResourcePath(resource):
 	else:
 		return resource
 
+# WindPower's magical unicode method
+def u(s):
+	if type(s) is type(u''):
+		return s
+	if type(s) is type(''):
+		try:
+			return unicode(s)
+		except:
+			try:
+				return unicode(s.decode('utf8'))
+			except:
+				try:
+					return unicode(s.decode('windows-1252'))
+				except:
+					return unicode(s, errors='ignore')
+	try:
+		return unicode(s)
+	except:
+		try:
+			return u(str(s))
+		except:
+			return s
+
 class curry(object):
 	def __init__(self, func, *args, **kwargs):
 		self._func = func
@@ -273,7 +296,7 @@ class DropLogView(QtGui.QWidget):
 					if print_string:
 						string += '%s, %s, %s, %s, %s\r\n' % (event['time'], event['event_type'], event['item'], event['item_id'], event['display_name'])
 			f = open(filename, 'wb')
-			f.write(string.encode('utf8'))
+			f.write(string)
 			f.close()
 
 	def openLink(self, url):
